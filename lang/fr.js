8bf8891527b5f7ce923931f0669a225c15eca2a4
Kabil-Boutou <kabil.boutou@gmail.com>
2020-08-17 17:07:44 +0100

    feat: Clean fr version

--- lang/fr.js
+++ lang/fr.js
@@ -10,7 +10,7 @@ export default {
       post: 'Full Stack',
       period: 'Juillet 2020 - présent',
       intro:
-        "Sortie du premier MVP Eqdom crédit consommation dans le cadre de la digitalisation et donnée un nouveau souffle à cet organisme qui a été fondée en 1974 . Animer les démonstrations, apportait de nouvelles propositions au PO l'or de chaque sprint planning et challenger l'équipe backend.",
+        "Entant que consultant Fullstack chez Eqdom, une société filiale du groupe SG, un leader des crédits/micro crédits. Ma mission est d'accompagner le client dans sa transformation digitale, améliorer le temps de crédit des dossiers avec un flux automatique et industrialisé, et permettant de donner au client son score en temps réel sous un cadre interactif et digital. Réaliser des jeux de démonstration pour les équipes client.",
       tasks: [],
       stack: 'React.Js, Styled Components, React intl, Gitlab.',
     },
@@ -21,7 +21,7 @@ export default {
       post: 'Développement Web',
       period: 'Août 2019 - Juin 2020',
       intro:
-        "Réalisation du front avec les tester unitaire, coordonner avec l'équipe back et mes collèges en front pour avoir un produit bien structuré et sous les normes de la digitale factory Société Générale. Création d'un poc application mobile pour les gérants de la direction générale et leur facilité la tache de communication entre les différentes filiales.",
+        "Réalisation du front avec les tester unitaire, coordonner avec les équipes client ainsi que mon équipe front pour avoir un produit structuré respectant les normes de la digitale factory Société Générale. Création d'un poc application mobile pour la direction générale assurant la facilité de communication entre les différentes filiales.",
       tasks: [],
       stack: 'Node.Js, Express.Js, React.Js/Next.Js, React Native, Postgres, GitLab, Jira, Slack, Zoom.',
     },
@@ -32,7 +32,7 @@ export default {
       post: 'Développement Web',
       period: 'Août 2018 - Juin 2019',
       intro:
-        "Création «from scratch» d'application web d'assurance auto et d'assurance voyage destinée au marché africain. Développement du Back-end d'application mobile d'assurance pour les particuliers. Ces plateformes ont pour but de faciliter la vente des produits d'assurance et garantir le relais entre les différents partenaires (banques, agrégateurs de paiements...etc)",
+        "Création «from scratch» d'une solution web assurance auto/voyage (B2C,B2B2C) pour le marché africain(UMOA). Développement du Back-end d'application mobile d'assurance pour les particuliers (B2C). Ces plateformes ont pour but de faciliter la vente des produits d'assurance et garantir le relais entre les différents partenaires (banques, agrégateurs de paiements...etc)",
       tasks: [],
       stack: 'Node.Js, Express.Js, React.Js, Socket.IO, MongoDB, Mongoose,Trello, Slack, Github.',
     },
@@ -43,7 +43,7 @@ export default {
       post: 'Consultant',
       period: 'Mai 2018 - présent',
       intro:
-        'Réalisation des plateformes B2B et B2C pour les différents client Maltem sous différant thème (Banque, Assurance, Commerce ...), en utilisent des technologies souple et performante tout en gardent l’interaction avec ces dernier simple et compréhensible.',
+        'Réalisation des plateformes B2B et B2C pour les différents client Maltem sous différant thème (Banque, Assurance, Commerce ...), en utilisant des technologies souple et performante tout en gardant une interaction utilisateur simple et compréhensible.',
       tasks: [],
       stack: 'Node.Js, Express.Js, GraphQL, React.Js/Next.Js, Redux/Apollo, FireBase/Postgres, Trello, Github.',
     },
@@ -87,11 +87,11 @@ export default {
   domaines: {
     title: 'Domaines',
     items: [
+      'Production/Industrie',
       'Distribution (PGI)',
-      'Personnel (PGI)',
-      'Production',
+      'SGRH (PGI)',
+      'Banke digitale',
       'E-commerce',
-      'E-Banking',
       'Marketing',
       'Assurance',
       'Services',
@@ -113,12 +113,12 @@ export default {
   intro: {
     title: 'Introduction',
     desc:
-      'Évoluer mes compétences informatiques et décisionnelles a toujours été l’un de mes objectifs ultimes, je cherche toujours à mettre en œuvre mes années d’expérience acquises et développé une personnalité capable de gérer des grandes équipes.',
+      'Évoluer mes compétences informatiques et décisionnelles a toujours été l’un de mes objectifs ultimes, je cherche toujours à mettre en œuvre mes années d’expérience acquises pour développé une personnalité capable de gérer de grandes équipes.',
   },
   about: {
     title: 'Mes qualities',
     desc: `
-    À part ce que j'ai déjà mentionné en haut, j'ai aussi un bon sens de l'humour.
-    Je réponds toujours présent pour écouter au problème des autres et pour donner des conseils s'il me le demande.`,
+   En dehors de tout ce qui est technique, mon sens d'humour me facilite l'integration des équipes.
+   Rigueur, persévérance, écoute et détermination des atouts qui répond toujours présent pour résoudre les problèmes et les difficultés rencontrer dans les différentes situations...`,
   },
 }
