788829aaf56ac8ab754670495972daf492f7735b
Kabil-Boutou <kabil.boutou@gmail.com>
2020-09-18 15:57:23 +0100

    perf: css purge

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
-const path = require('path')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+const withCss = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+const withPurgeCss = require('next-purgecss')
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-module.exports = withBundleAnalyzer({
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve('./'))
-    return config
-  },
-  pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
-  env: {
-    url: process.env.DOMAIN,
-    soundCloudApi: process.env.SOUNDCLOUD_API,
-  },
-})
+module.exports = withCss(withSass(withPurgeCss()))
