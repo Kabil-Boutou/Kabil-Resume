b689745ef644fa50454bbd6e701560b8e6e34f8a
Kabil-Boutou <kabil.boutou@gmail.com>
2020-09-18 17:17:44 +0100

    fix: slider bug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
-const withPurgeCss = require('next-purgecss')
+
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-module.exports = withCss(withSass(withPurgeCss()))
+module.exports = withCss(withSass())
