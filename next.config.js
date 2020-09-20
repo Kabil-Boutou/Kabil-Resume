59eafc44c43adfc011598b9d0e25eea693ccbf02
Kabil-Boutou <kabil.boutou@gmail.com>
2020-09-20 14:05:44 +0100

    perf: css purge config on production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-const withCss = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
 
-module.exports = withCss(withSass())
+if (process.env.NODE_ENV === 'production') {
+  const withCss = require('@zeit/next-css')
+  const withSass = require('@zeit/next-sass')
+  const withPurgeCss = require('next-purgecss')
+
+  module.exports = withCss(
+    withSass(
+      withPurgeCss({
+        purgeCss: {
+          whitelistPatterns: () => ['/aws*/', 'foldRight', 'foldLeft', 'button'],
+        },
+      })
+    )
+  )
+}
